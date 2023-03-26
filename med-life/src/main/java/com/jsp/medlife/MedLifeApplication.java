b0c62b154e80055b4ee249718c9b0171dffd8606
siddalinganaikcs <121216789+siddalinganaikcs@users.noreply.github.com>
2023-03-26 22:14:38 +0530

    Add files via upload

---
+++ med-life/src/main/java/com/jsp/medlife/MedLifeApplication.java
@@ -0,0 +1,15 @@
+ package com.jsp.medlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedLifeApplication.class, args);
+		
+		System.out.println("med-life started...");
+	}
+
+} 
